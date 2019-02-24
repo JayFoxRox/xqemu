450935b4de1c31d00a3960b778f38b0c67a2a4d7
Jannik Vogel <email@jannikvogel.de>
2019-02-24 06:45:51 +0100

    apu: Move debug features to separate header file

---
+++ hw/xbox/mcpx_apu_debug.h
@@ -0,0 +1,34 @@
+/*
+ * QEMU MCPX APU debug helpers
+ *
+ * Copyright (c) 2012 espes
+ * Copyright (c) 2019 Jannik Vogel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HW_MCPX_APU_DEBUG_H
+#define HW_MCPX_APU_DEBUG_H
+
+// #define DEBUG_MCPX_APU
+#ifdef DEBUG_MCPX_APU
+# define MCPX_APU_DPRINTF(format, ...)       printf("mcpx-apu: " format, ## __VA_ARGS__)
+#else
+# define MCPX_APU_DPRINTF(format, ...)       do { } while (0)
+#endif
+
+/* Generate a tone in the MIXBIN for every channel */
+#define GENERATE_MIXBIN_BEEP      0
+
+#endif
